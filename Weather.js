ec8a3340fd656b1eff550fe589ac9d6aeeaf7fb5
ptrapani <paul.t.trapani@gmail.com>
2014-12-22 20:24:31 -0500

    Adding test JSON data
    Adding hardcoded JSON file to test with

--- Weather.js
+++ Weather.js
@@ -1,9 +1,42 @@
-
 $(document).ready(function () {
 
-    $("#weather button").on("click", function () {
+    //$("#weather button").on("click", function () {
 
-    	alert("clicked!");	
+    $("#search-button").on("click", function () {
+
+        //value is hard coded for now
+
+/*
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.wunderground.com/api/6110d723ddc2a0c0/forecast/geolookup/conditions/q/Ga/atlanta.json',
+            data: {
+                get_param: 'value'
+            },
+            dataType: 'json',
+            success: function (data) {
+                alert("success!");
+                var weather = data;
+                console.log(data);
+
+                //this is where the JSON parsing will happen
+                //testing on static JSON file to avoid unneeded requests	
+
+            }
+        });
+*/
+
+        $.getJSON("atlanta.json", function (data) {
+            //parse current weather and set values in the html
+
+            console.log(data);
+            
+            $("#current-weather").html(data);
+
+            //parse forcast and set values in the html for each day
+            $("#forecast").html(data);
+
+        });
 
     });
 
