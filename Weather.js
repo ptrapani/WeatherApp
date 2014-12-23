6949ff4604b1e89c60387a331a2dcfb46eb584cc
ptrapani <paul.t.trapani@gmail.com>
2014-12-23 04:22:54 -0500

    Added load bar
    Added load bar during ajax call

--- Weather.js
+++ Weather.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    //$("#weather button").on("click", function () {
-
     $("form").on("submit", function (e) {
 
     	//prevents page refresh after form submission 
@@ -9,7 +7,7 @@ $(document).ready(function () {
 
     	var location = $("#location").val().split(',');
     	var url = "http://api.wunderground.com/api/6110d723ddc2a0c0/forecast/geolookup/conditions/q/" + location[1].trim()  + "/" + location[0].trim() + ".json";
-    	console.log(url);
+    	showLoadingBar();
 
         $.ajax({
             type: 'GET',
@@ -44,6 +42,25 @@ $(document).ready(function () {
 		    var info = Mustache.to_html(template, forecast);
 		    $("#forecast-container").show();
 		    $('#forecast-content').html(info);
+        }
+
+        function showLoadingBar(){
+
+			$('.js-loading-bar').modal({
+			  backdrop: 'static',
+			  show: false
+			});
+
+			  var $modal = $('.js-loading-bar'),
+			      $bar = $modal.find('.progress-bar');
+			  
+			  $modal.modal('show');
+			  $bar.addClass('animate');
+
+			  setTimeout(function() {
+			    $bar.removeClass('animate');
+			    $modal.modal('hide');
+			  }, 1000);			
         }
 
     });
